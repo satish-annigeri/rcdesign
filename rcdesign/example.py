56b0e7c2c38ebcd5129a27510887d10adf2a5809
Satish Annigeri <satish.annigeri@gmail.com>
2021-09-30 20:00:16 +0530

    Added example to root of package

---
+++ rcdesign/example.py
@@ -0,0 +1,24 @@
+"""Example 01
+
+Section: Rectangular 230 x 450
+Compression steel: Nil, Tension steel: 1 layer
+Output: xu and report of the section.
+"""
+from rcdesign.is456.material.concrete import ConcreteStressBlock, Concrete
+from rcdesign.is456.material.rebar import RebarHYSD, RebarLayer, RebarGroup, Stirrups
+from rcdesign.is456.section import RectBeamSection
+
+
+sb = ConcreteStressBlock("IS456 LSM", 0.002, 0.0035)
+m20 = Concrete("M20", 20, sb)
+fe415 = RebarHYSD("Fe 415", 415)
+
+t1 = RebarLayer([20, 16, 20], -35)
+steel = RebarGroup(fe415, [t1])
+sh_st = Stirrups(fe415, 2, 8, 150)
+
+sec = RectBeamSection(230, 450, m20, steel, sh_st, 25)
+print(sec)
+xu = sec.xu(0.0035)
+print(f"xu = {xu:.2f}")
+print(sec.report(xu, 0.0035))
