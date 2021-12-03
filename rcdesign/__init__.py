fa4447103cb3fec08034b37b67103b9862ea0124
Satish Annigeri <satish.annigeri@gmail.com>
2021-12-03 07:12:15 +0530

    Added docstring to rcdesign.__init__ for Flit 3.5.1

--- rcdesign/__init__.py
+++ rcdesign/__init__.py
@@ -1,3 +1,6 @@
+"""A Python package for analysis and design of reinforced concrete sections as per IS 456:2000, the Indian Standard code of practice for plain and reinforced concrete."""
+
 __version__ = "0.3.0"
+
 ecy = 0.002
 ecu = 0.0035
